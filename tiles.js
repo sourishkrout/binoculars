a0a26cef79b8be8094b755cf248c1cd9dd381645
Sebastian Tiedtke <sebastiantiedtke@gmail.com>
2012-01-25 01:40:54 -0800

    Give browsers 60sec browser cache before expiry

--- tiles.js
+++ tiles.js
@@ -1,7 +1,26 @@
 var fs = require('fs');
 var redis = require('redis');
+var Canvas = require('canvas');
 
 var Tiles = function(req, res, cb) {
+	var produceTile = function(count, high, cb) {
+		var acanvas = new Canvas(256, 256);
+		var ctx = acanvas.getContext('2d');
+		var ptg = 100 - (parseInt(count) / parseInt(high)) * 100;
+		var color = 'rgba(' + Math.floor(0.5+(255*ptg)/100) + ', ' + Math.floor(0.5+(255*(100-ptg))/100) + ', 0, 0.55)';
+		
+		console.log(count + ' / ' + high + ' = ' + ptg);
+		
+		ctx.fillStyle = color;
+		ctx.fillRect(0, 0, 256, 256);
+		
+		ctx.font = '36px Impact';
+		var te = ctx.measureText(count);
+		ctx.fillText(count, 128-te.width/2, 128);
+				
+		acanvas.toBuffer(function(err, buf) { cb(err, buf); });
+	};
+	
 	var client = redis.createClient();
 	var tile = req.param('tile', '');
 	
@@ -11,18 +30,13 @@ var Tiles = function(req, res, cb) {
 			count = reply;
 		}
 
-		console.log(tile + ': ' + count);
+		// console.log(tile + ': ' + count);
 		
-		res.writeHead(200, { 'Content-Type': 'image/png', 'X-Count': count });
-		fs.readFile('public/milk.png', function(err, data) {
-			if (err == null)
-				cb(null);
+		res.writeHead(200, { 'Content-Type': 'image/png', 'X-Count': count, 'max-age': 60 });
+		client.get('highest', function(err, high) {
+			produceTile(count, high, function(err, result) { cb(result); });
 		});
 	});
-};
-
-Tiles.prototype.produceTile = function(count) {
-	return null;
 };
 
 exports.create = function(req, res, cb) {
